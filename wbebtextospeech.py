8ec4356e24b5fbb0f27f6371644924c1fb10ad3a
DKMgaming <53993378+DKMgaming@users.noreply.github.com>
2024-11-01 10:24:02 +0700

    Update wbebtextospeech.py

--- wbebtextospeech.py
+++ wbebtextospeech.py
@@ -1,52 +1,27 @@
-import pyttsx3
-from pydub import AudioSegment
+from gtts import gTTS
 import tempfile
-import os
 import streamlit as st
+import os
 
-def text_to_speech_vietnamese(text, rate):
-    engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
+def text_to_speech_vietnamese(text, lang='vi'):
+    tts = gTTS(text=text, lang=lang, slow=False)
     
-    vietnamese_voice_id = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Speech\\Voices\\Tokens\\MSTTS_V110_viVN_An"
-    voice_found = False
-    for voice in voices:
-        if voice.id == vietnamese_voice_id:
-            engine.setProperty('voice', voice.id)
-            voice_found = True
-            break
-    
-    if not voice_found:
-        st.warning("Vietnamese voice not found. Using default voice.")
-    
-    engine.setProperty('rate', rate)
-    
-    # Tạo tệp WAV tạm thời
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_wav_file:
-        temp_wav_path = temp_wav_file.name
-    
-    engine.save_to_file(text, temp_wav_path)
-    engine.runAndWait()
-    
-    # Chuyển đổi sang MP3
+    # Tạo tệp MP3 tạm thời
     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_mp3_file:
         temp_mp3_path = temp_mp3_file.name
+    tts.save(temp_mp3_path)
     
-    audio = AudioSegment.from_wav(temp_wav_path)
-    audio.export(temp_mp3_path, format='mp3')
-    
-    os.remove(temp_wav_path)
     return temp_mp3_path
 
 st.title("Text to Speech App - Vietnamese")
 
 # Giao diện nhập liệu
 text = st.text_area("Enter the text you want to convert to speech")
-rate = st.number_input("Select the speaking rate", min_value=50, max_value=300, value=150)
+rate = st.number_input("Select the speaking rate (not applicable with gTTS)", min_value=50, max_value=300, value=150)
 
 if st.button("Convert to Speech"):
     if text.strip():
-        output_mp3_path = text_to_speech_vietnamese(text, rate)
+        output_mp3_path = text_to_speech_vietnamese(text)
         
         # Phát âm thanh và cung cấp tùy chọn tải về
         audio_file = open(output_mp3_path, "rb")
@@ -58,7 +33,7 @@ if st.button("Convert to Speech"):
             mime="audio/mp3"
         )
         
-        # Xóa tệp tạm
+        # Đóng và xóa tệp tạm
         audio_file.close()
         os.remove(output_mp3_path)
     else:
